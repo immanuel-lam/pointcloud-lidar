1a528ca0a933234926c4e9c73a583e777225bef0
immanuel <immanuel.c.lam@student.uts.edu.au>
2026-03-26 19:48:49 +1100

    Initial Commit

---
+++ pointcloud/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pointcloud
+//
+//  Created by Immanuel Lam on 26/3/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pointcloud/pointcloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  pointcloudApp.swift
+//  pointcloud
+//
+//  Created by Immanuel Lam on 26/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct pointcloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
